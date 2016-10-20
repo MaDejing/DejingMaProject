83356e5cc17fdb037011a3c4e542c1479c5a92f6
MaDejing <mdj940619@163.com>
2016-10-20 10:05:51 +0800

    添加新文件

---
+++ DejingMaProject/Project08/BottomTabViewController.swift
@@ -0,0 +1,26 @@
+//
+//  BottomTabViewController.swift
+//  DejingMaProject
+//
+//  Created by DejingMa on 16/10/20.
+//  Copyright © 2016年 DejingMa. All rights reserved.
+//
+
+import UIKit
+
+class BottomTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+		
+		
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
